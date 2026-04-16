ec23ba9fb3a83a15e537b1d009430d96a80412db
IsmailovNurmuhammed <ismailovnurmuhammed@gmail.com>
2026-04-16 13:06:24 +0600

    realize board script with TypeScript

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 "use strict";
-const whiteCell = ' ';
+const whiteCell = '  ';
 const blackCell = '██';
-console.log('TESSST');
+const size = 8;
+const drawBoard = () => {
+    let board = '';
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            ((row + col) % 2 === 0) ? board += whiteCell : board += blackCell;
+        }
+        board += '\n';
+    }
+    console.log(board);
+};
+drawBoard();
--- app.ts
+++ app.ts
@@ -1,3 +1,18 @@
-const whiteCell = ' ';
-const blackCell = '██';
-console.log('TESSST');
+const whiteCell: string = '  ';
+const blackCell: string = '██';
+const size: number = 8;
+
+const drawBoard = (): void => {
+  let board: string = '';
+
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      ((row + col) % 2 === 0) ? board += whiteCell : board += blackCell;
+    }
+    board += '\n';
+  }
+
+  console.log(board);
+}
+
+drawBoard();
